8a128da5bf8d06dfa48449c538d8bfe4cd75082d
ahmed91abbas <ahmed91abbas@hotmail.com>
2023-05-25 21:30:16 +0200

    convert to a class

--- rlg_trade_bumper.py
+++ rlg_trade_bumper.py
@@ -7,42 +7,54 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+CHROME_PATH = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+USER_PROFILE_PATH = "C:\\Users\\ahmed\\AppData\\Local\\Google\\Chrome\\User Data"
+RLG_USERNAME = 'Momtazzz'
 
-def init_driver():
-    chrome_path = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-    user_profile_path = "C:\\Users\\ahmed\\AppData\\Local\\Google\\Chrome\\User Data"
+class RLGTradeBumper:
+    def __init__(self, trade_count):
+        self.trade_count = trade_count
+        self.timer = None
 
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = chrome_path
-    chrome_options.add_argument(f'--user-data-dir={user_profile_path}')
+    def run(self):
+        self.driver = self.init_driver()
+        self.driver.get(f'https://rocket-league.com/player/{RLG_USERNAME}')
+        try:
+            self.bump_trades()
+        except Exception as e:
+            print(e)
+            self.stop()
 
-    driver = webdriver.Chrome(options=chrome_options)
-    return driver
+    def init_driver(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = CHROME_PATH
+        chrome_options.add_argument(f'--user-data-dir={USER_PROFILE_PATH}')
+        chrome_options.add_argument("--log-level=3")
+        driver = webdriver.Chrome(options=chrome_options)
+        return driver
 
-def bump_trades(driver, trade_count):
-    handled = set()
-    while len(handled) < trade_count:
-        driver.refresh()
-        driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(1)
-        buttons = driver.find_elements_by_tag_name('button')
-        for button in buttons:
-            data_alias = button.get_attribute('data-alias')
-            if button.text == 'Bump' and data_alias not in handled:
-                handled.add(data_alias)
-                button.click()
-                break
-    print(f"Bumped {trade_count} trades. Time: {time.ctime()}\n")
-    driver.refresh()
-    driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight/1.2);")
-    threading.Timer(905, bump_trades, [driver, trade_count]).start()
+    def bump_trades(self):
+        handled = set()
+        while len(handled) < self.trade_count:
+            self.driver.refresh()
+            self.driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+            buttons = self.driver.find_elements_by_tag_name('button')
+            for button in buttons:
+                data_alias = button.get_attribute('data-alias')
+                if button.text == 'Bump' and data_alias not in handled:
+                    handled.add(data_alias)
+                    button.click()
+                    break
+        print(f"Bumped {len(handled)} trades. Time: {time.ctime()}\n")
+        self.driver.refresh()
+        self.driver.execute_script(f"window.scrollTo(0, document.body.scrollHeight/1.2);")
+        self.timer = threading.Timer(902, self.bump_trades)
+        self.timer.start()
 
+    def stop(self):
+        self.timer.cancel()
+        self.driver.quit()
 
 if __name__ == '__main__':
-    driver = init_driver()
-    driver.get('https://rocket-league.com/player/Momtazzz')
-    try:
-        bump_trades(driver, 15)
-    except:
-        pass
-    driver.quit()
+    RLGTradeBumper(15).run()
